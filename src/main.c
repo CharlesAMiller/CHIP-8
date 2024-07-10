79e3fa9b2814205555b8d8faa8e495252a35a9f4
Charlie Miller <charlie@cenithinnovations.com>
2024-07-10 17:19:02 -0400

    Implementing more commands. Refactoring some.

--- src/main.c
+++ src/main.c
@@ -11,6 +11,14 @@
 #include <string.h>
 #include <stdint.h>
 
+// Dumb stupid temporary thing. Hopefully makes it more portable for testing.
+#define APP
+
+#ifdef APP
+#include <stdlib.h>
+#include <time.h>
+#endif
+
 ///
 /// Constants
 ///
@@ -20,14 +28,17 @@
 #define SCREEN_W 64
 #define SCREEN_H 32
 #define SCREEN_BYTES ((SCREEN_W * SCREEN_H) / __CHAR_BIT__)
+#define DIGIT_SPRITES_OFFSET 0
 
 #define H_OFFSET (SCREEN_H / __CHAR_BIT__)
 
 #define UNSET_PIXEL '-'
-#define SET_PIXEL '#' 
+#define SET_PIXEL '#'
 
 // 4 KB
 #define RAM_SIZE 4096
+
+#define PROGRAM_OFFSET 0x200
 
 ///
 /// State
@@ -59,7 +70,42 @@ enum op_type
     DISPLAY,
     IF_EQ,
     IF_NEQ,
+    IF_EQ_REG,
+    SET_REG_BY_REG,
+    OR,
+    AND,
+    XOR,
+    ADD_BY_REG,
+    SUB,
+    SHIFT_RIGHT,
+    SUBN,
+    SHIFT_LEFT,
+    SKIP_NEQ,
+    BNNN,
+    RANDOM,
+    GET_DELAY,
+    GET_KEY,
+    SET_DELAY,
+    SET_AUDIO,
+    ADVANCE_I,
+    SET_I_HEX_SPRITE,
+    BCD,
+    REG_DUMP,
+    REG_LOAD,
 };
+
+enum op_type op_type_lookup[] = {
+    [1] = JUMP,
+    [3] = IF_EQ,
+    [4] = IF_NEQ,
+    [5] = IF_EQ_REG,
+    [6] = SET_REG,
+    [7] = ADD_REG,
+    [9] = SKIP_NEQ,
+    [0xA] = SET_I_REG,
+    [0xB] = BNNN,
+    [0xC] = RANDOM,
+    [0xD] = DISPLAY};
 
 typedef struct op
 {
@@ -121,6 +167,10 @@ int main(int argc, char *argv[])
     u_int8_t instruction[2] = {0, 0};
     init_state(&state);
 
+#ifdef APP
+    srand(time(0));
+#endif
+
     while (1)
     {
         fetch(&state, instruction);
@@ -165,39 +215,87 @@ void decode(u_int8_t instruction[2], op *decoded_op)
         case 0xE0:
             decoded_op->type = CLEAR_DISPLAY;
             break;
-        default: 
-            printf("NOT YET IMPLEMENTED");
-            break;
         }
         break;
 
     case 1:
-        decoded_op->type = JUMP;
+    case 3 ... 7:
+    case 9 ... 0xD:
+        decoded_op->type = op_type_lookup[op_type_major];
         break;
 
-    case 3: 
-        decoded_op->type = IF_EQ;
+    case 8:
+        switch (instruction[1] & 0x0F)
+        {
+        case 0:
+            decoded_op->type = SET_REG_BY_REG;
+            break;
+
+        case 1:
+            decoded_op->type = OR;
+            break;
+
+        case 2:
+            decoded_op->type = AND;
+            break;
+
+        case 3:
+            decoded_op->type = XOR;
+            break;
+
+        case 4:
+            decoded_op->type = ADD_BY_REG;
+            break;
+
+        case 5:
+            decoded_op->type = SUB;
+            break;
+
+        case 6:
+            decoded_op->type = SHIFT_RIGHT;
+            break;
+
+        case 7:
+            decoded_op->type = SUBN;
+            break;
+
+        case 8:
+            decoded_op->type = SHIFT_LEFT;
+            break;
+        }
         break;
 
-    case 4:
-        decoded_op->type = IF_NEQ;
-        break;
-
-    case 6:
-        decoded_op->type = SET_REG;
-        break;
-
-    case 7:
-        decoded_op->type = ADD_REG;
-        break;
-
-    case 0xA:
-        decoded_op->type = SET_I_REG;
-        break;
-
-    case 0xD:
-        decoded_op->type = DISPLAY;
-        break;
+    case 0xF:
+        switch (instruction[1] & 0xFF)
+        {
+        case 0x07:
+            decoded_op->type = GET_DELAY;
+            break;
+        case 0x0A:
+            decoded_op->type = GET_KEY;
+            break;
+        case 0x15:
+            decoded_op->type = SET_DELAY;
+            break;
+        case 0x18:
+            decoded_op->type = SET_AUDIO;
+            break;
+        case 0x1E:
+            decoded_op->type = ADVANCE_I;
+            break;
+        case 0x29:
+            decoded_op->type = SET_I_HEX_SPRITE;
+            break;
+        case 0x33:
+            decoded_op->type = BCD;
+            break;
+        case 0x55:
+            decoded_op->type = REG_DUMP;
+            break;
+        case 0x65:
+            decoded_op->type = REG_LOAD;
+            break;
+        }
 
     default:
         printf("Instruction with op_type: %i not yet implemented", op_type_major);
@@ -237,6 +335,87 @@ void execute(op *decoded_op, state *state)
     case IF_NEQ:
         state->PC += (*x != decoded_op->nn) ? 2 : 0;
         break;
+    case IF_EQ_REG:
+        state->PC += (*x == *y) ? 2 : 0;
+        break;
+    case SET_REG_BY_REG:
+        *x = *y;
+        break;
+    case OR:
+        *x |= *y;
+        break;
+    case AND:
+        *x &= *y;
+        break;
+    case XOR:
+        *x ^= *y;
+        break;
+    case ADD_BY_REG:
+        // Carry
+        state->V[0xF] = ((u_int8_t)(*x + *y) < *x || ((u_int8_t)(*x + *y)) < *y);
+        *x += *y;
+        break;
+    case SUB:
+        // Borrow
+        state->V[0xF] = (*x > *y);
+        *x -= *y;
+        break;
+    case SHIFT_RIGHT:
+        state->V[0xF] = *x & 0x01;
+        *x >>= 1;
+        break;
+    case SUBN:
+        state->V[0xF] = (*y > *x);
+        *x = *y - *x;
+        break;
+    case SHIFT_LEFT:
+        state->V[0xF] = (*x & 0b10000000) >> 7;
+        *x <<= 1;
+        break;
+    case SKIP_NEQ:
+        state->PC += (*x != *y) ? 2 : 0;
+        break;
+    case BNNN:
+        state->PC = decoded_op->nnn + state->V[0];
+        break;
+    case RANDOM:
+#ifdef APP
+        *x = ((u_int8_t)rand() % 256) & decoded_op->nn;
+#endif
+        break;
+    case GET_DELAY:
+        *x = state->delay_timer;
+        break;
+    case GET_KEY:
+#ifdef APP
+        scanf("%c", x);
+#endif
+        break;
+    case SET_DELAY:
+        state->delay_timer = *x;
+        break;
+    case SET_AUDIO:
+        state->audio_timer = *x;
+        break;
+    case ADVANCE_I:
+        state->I += *x;
+        break;
+    case SET_I_HEX_SPRITE:
+        state->I = DIGIT_SPRITES_OFFSET + (*x * 5);
+        break;
+    case BCD:
+        state->memory[state->I] = (*x / 100) % 10;
+        state->memory[state->I + 1] = (*x / 10) % 10;
+        state->memory[state->I + 2] = *x % 10;
+        break;
+    case REG_DUMP:
+        for (int i = 0; i < REGISTER_COUNT; i++)
+            state->memory[state->I + i] = state->V[i];
+        break;
+    case REG_LOAD:
+        for(int i = 0; i < REGISTER_COUNT; i++)
+            state->V[i] = state->memory[state->I + i];
+        break;
     default:
         printf("Instruction with op_type %i not yet implemented", decoded_op->type);
     }
@@ -268,7 +447,6 @@ void display(state *state, op *decoded_op)
 
 void decode_nnn(u_int8_t instruction[2], u_int16_t *nnn)
 {
-    // TODO: Change usage of chars to u_int8_t? This would save us the trouble of casting
     *nnn = (((instruction[0]) & 0x0F) << 8) | instruction[1];
 }
 
@@ -290,6 +468,30 @@ void init_state(state *state)
     for (int i = 0; i < RAM_SIZE; i++)
         state->memory[i] = 0;
 
+    u_int8_t digit_sprites_data[] = {
+        0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+        0x20, 0x60, 0x20, 0x20, 0x70, // 1
+        0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+        0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+        0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+        0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+        0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+        0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+        0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+        0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+        0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+        0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+        0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+        0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+        0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+        0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+    };
+
+    // Store sprite representation of hex digits to memory
+    for (int i = 0; i < 16 * 5; i++)
+        state->memory[DIGIT_SPRITES_OFFSET + i] = digit_sprites_data[i];
+
+    /*
     // Set up program memory with a dummy program
     // clear screen
     state->memory[0] = 0x00;
@@ -324,11 +526,49 @@ void init_state(state *state)
     // jump (12)
     state->memory[20] = 0x10;
     state->memory[21] = 0x0C;
+    */
+
+    /*
+    // Set reg (1, 1)
+    state->memory[0] = 0x61;
+    state->memory[1] = 0x01;
+    // Set reg (2, 2)
+    state->memory[2] = 0x62;
+    state->memory[3] = 0x02;
+    // SNEQ (1, 2)
+    state->memory[4] = 0x91;
+    state->memory[5] = 0x20;
+    // Set reg (3, 1)
+    state->memory[6] = 0x63;
+    state->memory[7] = 0x01;
+    // Jump (0)
+    state->memory[8] = 0x10;
+    state->memory[9] = 0x00;
+    */
+    // Set reg 1 to 123
+    state->memory[PROGRAM_OFFSET] = 0x61;
+    state->memory[PROGRAM_OFFSET + 1] = 0x7B;
+    // Set I to 0x200 + something (write after program)
+    state->memory[PROGRAM_OFFSET + 2] = 0xA2;
+    state->memory[PROGRAM_OFFSET + 3] = 0x06;
+    // BCD
+    state->memory[PROGRAM_OFFSET + 4] = 0xF1;
+    state->memory[PROGRAM_OFFSET + 5] = 0x33;
+
+    state->memory[PROGRAM_OFFSET + 10] = 0xF1;
+    state->memory[PROGRAM_OFFSET + 11] = 0x0A;
+
+    // state->memory[PROGRAM_OFFSET + 2] = 0xF1;
+    // state->memory[PROGRAM_OFFSET + 3] = 0x29;
+    // state->memory[PROGRAM_OFFSET + 4] = 0xF1;
+    // state->memory[PROGRAM_OFFSET + 5] = 0x0A;
 
     // for (int i = 0; i < SCREEN_BYTES; i++)
     // state->memory[2000 + i] = 0xFF;
 
-    state->PC = 0;
+    state->PC = PROGRAM_OFFSET;
+    state->I = 0;
+
     for (int i = 0; i < REGISTER_COUNT; i++)
         state->V[i] = 0;
 }
@@ -342,8 +582,8 @@ void print_screen(u_int8_t screen[SCREEN_BYTES])
             printf("\n%d\t", i / H_OFFSET);
 
         u_int8_t pixels = screen[i];
-        for (int i = 8; i > 0; i--, pixels <<= 1) 
-            printf("%c", ((pixels & pixel_mask) != 0) ? SET_PIXEL: UNSET_PIXEL);
+        for (int i = 8; i > 0; i--, pixels <<= 1)
+            printf("%c", ((pixels & pixel_mask) != 0) ? SET_PIXEL : UNSET_PIXEL);
     }
 }
 
@@ -376,13 +616,41 @@ void print_op(op *op)
     case DISPLAY:
         strcpy(op_type_str, "DISPLAY");
         break;
-    
+
     case IF_EQ:
         strcpy(op_type_str, "IF EQUAL");
         break;
 
     case IF_NEQ:
         strcpy(op_type_str, "IF NOT EQUAL");
+        break;
+
+    case IF_EQ_REG:
+        strcpy(op_type_str, "IF REG EQUAL");
+        break;
+
+    case SET_REG_BY_REG:
+        strcpy(op_type_str, "SET X TO Y");
+        break;
+
+    case ADD_BY_REG:
+        strcpy(op_type_str, "ADD X TO Y");
+        break;
+
+    case SUB:
+        strcpy(op_type_str, "SUB Y FROM X");
+        break;
+
+    case SHIFT_RIGHT:
+        strcpy(op_type_str, "SHIFT X RIGHT");
+        break;
+
+    case SUBN:
+        strcpy(op_type_str, "SUB X FROM Y");
+        break;
+
+    case SHIFT_LEFT:
+        strcpy(op_type_str, "SHIFT X LEFT");
         break;
 
     default:
